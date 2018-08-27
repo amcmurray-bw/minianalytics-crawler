2b5959f0677086ee99c9f50eb8cd51e840b18c5b
Aimee McMurray <amcmurray@brandwatch.com>
2018-08-27 14:47:30 +0200

    adding simple Filter class

---
+++ minianalytics-api/src/main/java/amcmurray/bw/Filter.java
@@ -0,0 +1,14 @@
+package amcmurray.bw;
+
+public class Filter {
+
+    private String languageCode;
+
+    public String getLanguageCode() {
+        return languageCode;
+    }
+
+    public void setLanguageCode(String languageCode) {
+        this.languageCode = languageCode;
+    }
+}
